a8568eae637eeddc1de09ad11fca34c1a28b5a51
wassimnassour <nassourwassim@gmail.com>
2020-10-17 11:40:30 +0100

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -104,5 +104,11 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
+      },
+    },
   ],
 };
